bbed05d8c8231d0a8c90776173dc86e51fdcacf6
Bibek74 <bbekshah789@gmail.com>
2026-02-01 08:25:04 +0545

    refactor(model): update user schema/model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,12 +1,14 @@
+//user.model.ts
 import mongoose, { Document, Schema } from "mongoose";
 import { UserType } from "../types/user.type";
 const UserSchema: Schema = new Schema<UserType>(
     {
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
-        username: { type: String, required: true, unique: true },
+        name: { type: String, required: true, unique: true },
         firstName: { type: String },
         lastName: { type: String },
+        image: { type: String },
         role: {
             type: String,
             enum: ['user', 'admin'],
